91daed8233a88ec24375bdf8abeb40d2fe32147a
juliusamecke <julius.amecke@googlemail.com>
2026-03-30 21:39:23 +0200

    Create main.jsx

---
+++ SRC/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../baugenehmigung-mvp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
