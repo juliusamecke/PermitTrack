ccaba1673b44bd2b0bc3fc4ce82a80e0ab740acf
juliusamecke <julius.amecke@googlemail.com>
2026-03-30 21:45:56 +0200

    Update main.jsx

--- SRC/main.jsx
+++ SRC/main.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../baugenehmigung-mvp.jsx";
+import App from "./baugenehmigung-mvp.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
